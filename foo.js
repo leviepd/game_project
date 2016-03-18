770cf2d3947ed4e48ac651d094caed2a39b5b405
Joe Hitchens <joe@sleepless.com>
2016-03-18 12:58:54 -0700

    -

--- foo.js
+++ foo.js
@@ -1,5 +1,49 @@
 
 
+sega = {
+	name: "SEGA",
+	price: 7500,
+}
+snes = {
+	name: "SNES",
+	price: 8000,
+}
+products = [ sega, snes ];
+
+
+order = {
+	customer: "Billy Bob",
+	lines: [
+		{
+			product: sega,
+			qty: 1,
+			subtotal: 7500,
+		},
+		{
+			product: snes,
+			qty: 2,
+			subtotal: 160000,
+		}
+	],
+	total: 175000;
+}
+
+
+
+
+
+
+new_line = {
+	id: 1,
+	product_id: 2,
+	sub_total: 0,
+}
+
+
+
+
+
+
 
 block1 = {
 	name: "Room 1",
