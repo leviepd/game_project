8ab98aa9245a88ab06ef16cfcdc6735bdbf8029a
Levi Dobson <levi@sleepless.com>
2016-04-13 12:33:34 -0700

    -

--- search.js
+++ search.js
@@ -40,23 +40,21 @@ function show_order_search(c) {
 				write("No existing Record(s)");
 			}
 			else {
-				r.records.forEach(function(rec)) {
+				r.records.forEach(function(rec) {
 					var id = rec.id;
-					db.sql("select customer from orders where id = ?", [id], function(r) { // get the customer out of the record with this id
-						var customer = r.records[0].customer; // 0 the first object in the array... selected the customer out of the object
+					db.sql("select customer from orders where id = ?", [id], function(r2) { // get the customer out of the record with this id
+						var customer = r2.records[0].customer; // 0 the first object in the array... selected the customer out of the object
 						write("customer: " + customer + id); 
-						db.sql("select * from items where order_id = ?", [id], function(r) { 
+						db.sql("select * from items where order_id = ?", [id], function(r3) { 
 						// gets objects that have the same order_id and allows me access to data within the objects, unique ids but same order_id
-							write(r.error);
-							write(r.records.length);	
-							var items = r.records;
+							var items = r3.records;
 							for(i = 0; i < items.length; i++) {
-								write(items[i].product + items[i].price + items[i].qty); // price qty id	
+								write("Product: " + items[i].product + " Price: " + items[i].price + " Quantity: " + items[i].qty); // price qty id	
 							}
 							write("");
 						});
 					});
-				}
+				});
 			}
 		}
 	});
